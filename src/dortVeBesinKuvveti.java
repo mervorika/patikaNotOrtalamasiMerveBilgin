b091e3e275f72db6b506e8573985ca1e030c4895
Merve Bilgin <mervemermer243@gmail.com>
2023-09-24 19:43:14 +0300

    Patika Week1

---
+++ src/dortVeBesinKuvveti.java
@@ -0,0 +1,2 @@
+public class dortVeBesinKuvveti {
+}
--- src/faktoriyelKombinasyonMerveBilgin.java
+++ src/faktoriyelKombinasyonMerveBilgin.java
@@ -1,2 +1,17 @@
+
 public class faktoriyelKombinasyonMerveBilgin {
+    public static void main(String[] args){
+        System.out.println("4 ün 2'li kombinasyonu: " + kombinasyon(4,2));
+    }
+    public static int kombinasyon(int n, int r){
+        //C(n,r) = n! / (r! * (n-r)!)
+        return  faktoriyel(n) / ( faktoriyel(r) * faktoriyel(n-r));
+    }
+    public static int faktoriyel(int n){
+        int toplam = 1;
+        for( int i = 1; i<= n; i++ ){
+            toplam = toplam * i;
+        }
+        return toplam;
+    }
 }
