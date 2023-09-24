15f20e1d2c7e5f9f86e02189f3bd36d84ce8f2e8
Merve Bilgin <mervemermer243@gmail.com>
2023-09-24 19:45:33 +0300

    Patika Week1

--- src/dortVeBesinKuvveti.java
+++ src/dortVeBesinKuvveti.java
@@ -1,2 +1,17 @@
+
+import java.util.Scanner;
+
 public class dortVeBesinKuvveti {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Bir sayı giriniz: ");
+        int length = scanner.nextInt();
+        for(int i = 1; i< length; i*=4){
+            System.out.print(i+" ");
+        }
+        System.out.println();
+        for(int i = 1; i< length; i*=5){
+            System.out.print(i+" ");
+        }
+    }
 }
